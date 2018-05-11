b12a7efc7d1a639e76d3982ad3e8cbe20231d31a
zacarias <zacariassamaniego@gmail.com>
2018-05-11 14:27:46 -0300

    First commit

---
+++ src/main/java/com/tutorialspoint/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint.test;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "helloWorld", eager = true)
+@RequestScoped
+public class HelloWorld {
+	public HelloWorld() {
+      System.out.println("HelloWorld started!");
+      System.out.println("!!!!!!!!!!!!!!");
+   }
+	
+   public String getMessage() {
+      return "Hello World!";
+   }
+}
